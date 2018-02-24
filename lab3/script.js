e0044bb1371995a8b1d69a8b4abbb3d22c72a258
Emily Morrow <emilychandler365@gmail.com>
2018-02-24 09:13:41 -0700

    add buttons and ratings

--- lab3/script.js
+++ lab3/script.js
@@ -1,65 +1,123 @@
-$(document).ready(function() {
-	var i = 1;
-	var dogs;
-	
-	var myurl= "https://api.rescuegroups.org/rest/?key=8oqHMgcE&type=animals&limit=20";
-	$.ajax({
-		type:"GET",
-	    url : myurl,
-	    dataType : "xml",
-	    success : function(xml) {
-	    	showPage();
-			dogs = xml.getElementsByTagName("pet");
-			console.log(dogs);
-	    	showDog(dogs[1]);
-    	}
-	});
+Vue.component('star-rating', VueStarRating.default);
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    number: '',
+    max:'',
+    current: {},
+    loading: true,
+    addedName: '',
+    addedComment: '',
+    comments: {},
+    ratings: {},
+    rating:0,
+  },
+  created: function() {
+    this.xkcd();
+  },
+  computed: {
+    month: function() {
+      var month = new Array;
+      if (this.current.month === undefined)
+	return '';
+      month[0] = "January";
+      month[1] = "February";
+      month[2] = "March";
+      month[3] = "April";
+      month[4] = "May";
+      month[5] = "June";
+      month[6] = "July";
+      month[7] = "August";
+      month[8] = "September";
+      month[9] = "October";
+      month[10] = "November";
+      month[11] = "December";
+      return month[this.current.month - 1];
+    }
+  },
 
+  watch: {
+    number: function(value,oldvalue) {
+      if (oldvalue === '') {
+		this.max = value;
+      } else {
+		this.xkcd();
+      }
+    },
+  },
+  methods: {
+    xkcd: function() {
+      fetch('https://xkcd.now.sh/' + this.number).then(response => {
+		return response.json();
+      }).then(json => {
+		this.current = json;
+		this.loading = false;
+		this.number = json.num;
+		if (this.ratings[this.number] == null) this.rating = 0;
+		return true;
+      }).catch(err => {
+      	this.number = this.max;
+      });
 
+    },
+    previousComic: function() {
+      this.number = this.current.num - 1;
+      // this.rating = this.ratings[this.number].avg;
+    },
+    nextComic: function() {
+      this.number = this.current.num + 1;
+      // this.rating = this.ratings[this.number].avg;
+      // console.info(rating);
+    },
+ 	getRandom: function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
 
+      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive 
+    },
+    randomComic: function() {
+      this.number = this.getRandom(0,this.max);
+      // this.rating = this.ratings[this.number].avg;
+    },
+    addComment: function() {
+     	if (!(this.number in this.comments))
+			Vue.set(app.comments, this.number, new Array);
+		var currentDate = new Date()
+		date = currentDate.toLocaleString();
+		this.comments[this.number].push({author:this.addedName,text:this.addedComment,date:date});
+		this.addedName = '';
+		this.addedComment = '';
+    },
+    addRating: function(rating) {
+    	// console
+    	// if (!(this.number in this.ratings))
+    	// 	Vue.set(app.ratings, this.number, new Array);
+    	if (this.ratings == null) this.ratings == new Array();
+    	oldRating = this.ratings[this.number];
+    	if (oldRating == null) 
+    		this.ratings[this.number] = {num:1,avg:rating};
 
-	function showPage() {
-	  document.getElementById("loader").style.display = "none";
-	  document.getElementById("myDiv").style.display = "block";
-	}
-
-	var showDog = function(dog) {
-		console.log("hey")
-		document.getElementById("name").innerHTML = dog.getElementsByTagName("name")[0].childNodes[0].nodeValue
-			+", " + "<span class=\"age\">" + dog.getElementsByTagName("age")[0].childNodes[0].nodeValue + "</span>";
-		var img = dog.getElementsByTagName("pic1")[0].childNodes[0].nodeValue;
-		$("#image").attr("src", img);
-
-		console.log(dog.getElementsByTagName("pic1")[0].childNodes[0].nodeValue);
-
-	}
-
-	function nextItem() {
-		i = i + 1;
-		if (i > 19) i = 1;
-		showDog(dogs[i]);
-	}
-
-	var like = document.getElementById("like");
-	var dislike = document.getElementById("dislike");
-
-	dislike.style.cursor = 'pointer';
-	like.style.cursor = 'pointer';
-
-	// dislike.onclclick
-	// like.onclick = nextItem;
-
-	$("#like").click(function() {
-		nextItem();
-	})
-
-	$("#dislike").click(function() {
-		$("#photo").effect("shake",300);
-	})
-
-
-	
-	
-
+    	else {
+	    	num = oldRating.num;
+	    	avg = oldRating.avg;
+	    	newAvg = (avg + rating) / (num + 1);
+	    	console.info(newAvg);
+	    	this.ratings[this.number] = {num:num+1,avg:newAvg};
+	    	this.rating = newAvg;
+   	 	}
+    },
+    lastComic: function() {
+    	console.info(this.max);
+    	this.number = this.max;
+    	// this.rating = this.ratings[this.number].avg;
+    },
+    firstComic: function() {
+    	this.number = 1;
+    	// this.rating = this.ratings[this.number].avg;
+    },
+  }
 });
+
+
+
